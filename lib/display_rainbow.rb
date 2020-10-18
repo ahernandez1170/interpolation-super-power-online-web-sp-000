22358742a72272bfa5f1bf457b3034b72eba61fe
Austin Hernandez <ahernandez1170@gmail.com>
2020-10-18 01:51:08 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,7 +6,3 @@ def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
 
-colors = ['green', 'black', 'yellow']
-def fart(forestcolor)
-  puts "g: #{forestcolor(0)}. b: #{forestcolor(1)}, y: #{forestcolor(2)}"
-  end"
